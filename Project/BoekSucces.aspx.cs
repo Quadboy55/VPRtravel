0be6602f85dc99a7b045c3bae7bdb124772942ef
Van Parijs Anthony <anthony.vanparijs@student.katho.be>
2013-05-15 13:18:32 +0200

    updatee

--- Project/BoekSucces.aspx.cs
+++ Project/BoekSucces.aspx.cs
@@ -28,4 +28,19 @@ public partial class _Default : System.Web.UI.Page
         Session["VPR_grdRit"] = null;
         Session["VPR_vertrek/aankomst"] = null;
     }
+
+    private void setGridBestemming()
+    {
+        Dictionary<int, PlaatsData> plaatsData = Hulp.getBestemmingData();
+
+        for (int r = 0; r < grdRitten.Rows.Count; r++)
+        {
+            for (int i = 2; i < 4; i++)
+            {
+                int id = Int32.Parse(grdRitten.Rows[r].Cells[i].Text);
+                grdRitten.Rows[r].Cells[i].Text = plaatsData[id].naam;
+
+            }
+        }
+    }
 }
